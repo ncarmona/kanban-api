3a1ba2c0a56fb681c0513f8f176297de5964b5e2
Noel Carmona <hello@noelcarmona.com>
2022-03-03 17:54:43 +0100

    [USERS] User model implementation

---
+++ domain/models/userModel/user.model.test.ts
@@ -0,0 +1,114 @@
+import { IUser } from "../../interfaces/IUser"
+import { UserModel } from "./user.model"
+
+describe("User model", () => {
+  let userModel: UserModel
+  const user: IUser = {
+    id: "1",
+    created_at: new Date(),
+    deleted: false,
+    disabled: false,
+    email: "mail@fake.com",
+    name: "user name",
+    password: "secretpassword",
+    modified_at: new Date(),
+  }
+  beforeEach(() => (userModel = new UserModel(user)))
+  it("User without ID should no return user with ID", () => {
+    const user: IUser = {
+      //id: "1",
+      created_at: new Date(),
+      deleted: false,
+      disabled: false,
+      email: "mail@fake.com",
+      name: "user name",
+      password: "secretpassword",
+      modified_at: new Date(),
+    }
+    const userModel: UserModel = new UserModel(user)
+    const userID = userModel.getID()
+
+    expect(userID).toBeUndefined()
+  })
+  it("User with ID should no return user with ID", () => {
+    const userID = userModel.getID()
+
+    expect(userID).toBe(user.id)
+  })
+  it("Set ID should assign an ID to user", () => {
+    const expectedID = "2"
+    userModel.setID(expectedID)
+
+    expect(userModel.getID()).toBe(expectedID)
+  })
+
+  it("User name must be the same in the model and the IUser", () => {
+    expect(userModel.getName()).toStrictEqual(user.name)
+  })
+  it("User name must be assigned with setName", () => {
+    const name = "user2"
+    userModel.setName(name)
+
+    expect(userModel.getName()).toStrictEqual(name)
+  })
+
+  it("Password must be the same in the model and the IUser", () => {
+    expect(userModel.getPassword()).toStrictEqual(user.password)
+  })
+  it("User password must be assigned with setPassword", () => {
+    const password = "secretP"
+    userModel.setPassword(password)
+
+    expect(userModel.getPassword()).toStrictEqual(password)
+  })
+
+  it("Email must be the same in the model and the IUser", () => {
+    expect(userModel.getEmail()).toStrictEqual(user.email)
+  })
+  it("User email must be assigned with setEmail", () => {
+    const email = "mail@mail.com"
+    userModel.setEmail(email)
+
+    expect(userModel.getEmail()).toStrictEqual(email)
+  })
+
+  it("Disabled must be the same in the model and the IUser", () => {
+    expect(userModel.getDisabled()).toStrictEqual(user.disabled)
+  })
+  it("User disabled must be assigned with setDisabled", () => {
+    const disabled = true
+    userModel.setDisabled(disabled)
+
+    expect(userModel.getDisabled()).toStrictEqual(disabled)
+  })
+
+  it("Deleted must be the same in the model and the IUser", () => {
+    expect(userModel.getDeleted()).toStrictEqual(user.deleted)
+  })
+  it("User deleted must be assigned with setDeleted", () => {
+    const deleted = true
+    userModel.setDeleted(deleted)
+
+    expect(userModel.getDeleted()).toStrictEqual(deleted)
+  })
+
+  it("Modified_at must be the same in the model and the IUser", () => {
+    expect(userModel.getModifiedAt()).toStrictEqual(user.modified_at)
+  })
+  it("User modified_at must be assigned with setModifiedAt", () => {
+    const modified_at = new Date()
+    userModel.setModifiedAt(modified_at)
+
+    expect(userModel.getModifiedAt()).toStrictEqual(modified_at)
+  })
+
+  it("Created_at must be the same in the model and the IUser", () => {
+    expect(userModel.getCreatedAt()).toStrictEqual(user.created_at)
+  })
+  it("User created_at must be assigned with setCreatedAt", () => {
+    const created_at = new Date()
+    userModel.setCreatedAt(created_at)
+
+    expect(userModel.getCreatedAt()).toStrictEqual(created_at)
+  })
+})
---
+++ domain/models/userModel/user.model.ts
@@ -0,0 +1,65 @@
+import { IUser } from "../../interfaces/IUser"
+
+export class UserModel {
+  private _user: IUser
+
+  constructor(user: IUser) {
+    this._user = { ...user }
+  }
+
+  public getID(): string | undefined {
+    return this._user.id
+  }
+  public setID(id: string) {
+    this._user.id = id
+  }
+
+  public getName(): string {
+    return this._user.name
+  }
+  public setName(name: string) {
+    this._user.name = name
+  }
+
+  public getPassword(): string {
+    return this._user.password
+  }
+  public setPassword(password: string) {
+    this._user.password = password
+  }
+
+  public getEmail(): string {
+    return this._user.email
+  }
+  public setEmail(email: string) {
+    this._user.email = email
+  }
+
+  public getDisabled(): boolean {
+    return this._user.disabled
+  }
+  public setDisabled(disabled: boolean) {
+    this._user.disabled = disabled
+  }
+
+  public getDeleted(): boolean {
+    return this._user.deleted
+  }
+  public setDeleted(deleted: boolean) {
+    this._user.deleted = deleted
+  }
+
+  public getModifiedAt(): Date {
+    return this._user.modified_at
+  }
+  public setModifiedAt(date: Date) {
+    this._user.modified_at = date
+  }
+
+  public getCreatedAt(): Date {
+    return this._user.created_at
+  }
+  public setCreatedAt(date: Date) {
+    this._user.created_at = date
+  }
+}
