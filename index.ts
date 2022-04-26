8ace967fcf56233ed7e97e3c89ee53f3d151e0a1
Noel Carmona <hello@noelcarmona.com>
2022-04-26 10:15:22 +0200

    [CONFIG] Added body-parser to app

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express"
+import bodyParser from "body-parser"
 import { router } from "./core/router"
 
 const app: Application = express()
+app.use(bodyParser.json())
 const port = 5000
 router(app)
 app.listen(port, () => console.log("it works"))
