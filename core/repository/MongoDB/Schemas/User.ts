692551db63d1e539ba9e0f974ff679dd35e9e2b4
Noel Carmona <hello@noelcarmona.com>
2022-04-27 13:29:19 +0200

    [AUTH] User schema for mongoDB

---
+++ core/repository/MongoDB/Schemas/User.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose"
+
+const schema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  disabled: {
+    type: Boolean,
+    default: true,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  modified_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  name: String,
+  photo: String,
+})
+
+const mongoDBUser = model("User", schema)
+
+export { mongoDBUser }
