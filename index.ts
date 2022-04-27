6a00bfd92ef365bab6f1c846801f78807683beb3
Noel Carmona <hello@noelcarmona.com>
2022-04-27 13:28:51 +0200

    [REPOSITORY] MongoDBConnector loaded.

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
+import { IConnector } from "./core/repository/connectors/IConnector"
+import { MongoDBConnector } from "./core/repository/connectors/MongoDBConnector"
 import express, { Application } from "express"
 import bodyParser from "body-parser"
 import { router } from "./core/router"
 
 const app: Application = express()
 app.use(bodyParser.json())
+
 const port = 5000
+let connector: IConnector
+
 router(app)
-app.listen(port, () => console.log("it works"))
+app.listen(port, async () => {
+  connector = new MongoDBConnector()
+  try {
+    await connector.link()
+  } catch (error) {
+    console.error(error)
+  }
+})
