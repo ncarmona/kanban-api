7f8c2f22ebafe968e876ff486c7a8069a1b56cc6
Noel Carmona <hello@noelcarmona.com>
2022-04-29 20:30:56 +0200

    [CORS] Applied cors to signup route.

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -4,6 +4,8 @@ import { IResponse } from "../core/routes/IResponse"
 import { IRoute } from "../core/routes/IRoute"
 import { AuthController } from "../infrastructure/controllers/authController"
 import { requiredParameters } from "../core/middlewares"
+import { allowAll } from "../core/cors"
+import cors from "cors"
 
 export class AuthRoutes implements IRoute {
   private readonly base_route: string
@@ -23,7 +25,10 @@ export class AuthRoutes implements IRoute {
   private createRoute(action: string) {
     const authController = new AuthController()
     const route = this.base_route + action
-    const middlewares = [requiredParameters(["email", "password"])]
+    const middlewares = [
+      requiredParameters(["email", "password"]),
+      cors(allowAll),
+    ]
 
     this.core.post(route, middlewares, async (req: Request, res: Response) => {
       const { email, password } = req.body
