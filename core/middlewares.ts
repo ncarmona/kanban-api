72faacd80bbfe86efd498278ae64d1a62ae78f3b
Noel Carmona <hello@noelcarmona.com>
2022-05-02 11:45:11 +0200

    [MIDDLEWARE] Fixed requiredParameters.

--- core/middlewares.ts
+++ core/middlewares.ts
@@ -4,8 +4,8 @@ import { missingParameters } from "./responses/responses"
 export function requiredParameters(requiredParameters: string[]) {
   return (req: Request, res: Response, next: NextFunction) => {
     const parameters: string[] = Object.keys(req.body)
-    const validParam: boolean = parameters.every((param: string) =>
-      requiredParameters.includes(param)
+    const validParam: boolean = requiredParameters.every((param: string) =>
+      parameters.includes(param)
     )
     const response: IResponse = missingParameters(
       parameters,
