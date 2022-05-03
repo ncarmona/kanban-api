da0a32ecc5f534631b2aa539dda5b3574b30afb9
Noel Carmona <hello@noelcarmona.com>
2022-05-03 10:54:38 +0200

    [CONTROLLER] Activation user.

--- infrastructure/controllers/authController.ts
+++ infrastructure/controllers/authController.ts
@@ -3,6 +3,8 @@ import {
   signupSuccessful,
   invalidSignupEmail,
   emailAlreadyExists,
+  activationSuccessful,
+  userAlreadyActivated,
 } from "@responses/authResponses"
 import { unexpectedError } from "@core/responses/responses"
 import { MongoDBAuthRepository } from "@repositories/mongoDBauthRepository"
@@ -12,6 +14,8 @@ import { IAuth } from "@core/models/IAuth"
 import { regexEmail } from "@utils/regExpressions"
 import { IUser } from "@interfaces/IUser"
 import { AuthUseCases } from "@usecases/authUseCases"
+import { send } from "@utils/mail"
+import Mail from "nodemailer/lib/mailer"
 
 export class AuthController {
   private readonly authUseCases: AuthUseCases
@@ -22,6 +26,24 @@ export class AuthController {
     this.authUseCases = new AuthUseCases(this.mongoDBAuthRepository)
   }
 
+  //TODO: ActivationURL must be generated using environment server url.
+  private async sendActivationEmail(user: IUser) {
+    const activationURL =
+      "http://localhost:5000/auth/activation?email=" +
+      user.email +
+      "&activation_token=" +
+      user.activation_token
+    const activationLink = "<a href=" + activationURL + ">activate</a>"
+    const html =
+      "In order to use the kanban app you must activate our account with the following link: " +
+      activationLink
+    const mailBody: Mail.Options = {
+      to: user.email,
+      subject: "Kanban activation email",
+      html,
+    }
+    await send(mailBody)
+  }
   async signup(auth: IAuth): Promise<IResponse> {
     if (!regexEmail(auth.email)) invalidSignupEmail({ email: auth.email })
 
@@ -30,13 +52,29 @@ export class AuthController {
     try {
       const user: UserModel = await this.authUseCases.signup(auth)
       const userObject: IUser = user.getModel()
+      this.sendActivationEmail(userObject)
+
       delete userObject.password
+      delete userObject.activation_token
+
       response = signupSuccessful(userObject)
     } catch (error) {
       if (error === "Email already exists.") response = emailAlreadyExists()
       else response = unexpectedError()
     }
 
+    return response
+  }
+  async activation(auth: IAuth): Promise<IResponse> {
+    let response: IResponse
+
+    try {
+      const user: UserModel = await this.authUseCases.activate(auth)
+      response = activationSuccessful(user.getEmail())
+    } catch (error) {
+      if (error === "User already activated") response = userAlreadyActivated()
+      else response = unexpectedError()
+    }
     return response
   }
 }
