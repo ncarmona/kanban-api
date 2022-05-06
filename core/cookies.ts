b0c9373ecb8e8ac7639009feaaa944e8f69078ca
Noel Carmona <hello@noelcarmona.com>
2022-05-06 19:41:41 +0200

    [AUTH] Generate auth cookie when login

--- core/cookies.ts
+++ core/cookies.ts
@@ -2,10 +2,14 @@ import { IAuth } from "./models/IAuth"
 import { generateAuthToken } from "./auth"
 import { CookieOptions, Response } from "express"
 export function generateAuthCookie(res: Response, auth: IAuth): void {
+  const expires = new Date()
+  expires.setTime(new Date().getTime() + 3600000 * 24 * 14)
+
   const options: CookieOptions = {
     sameSite: "lax",
     httpOnly: true,
     secure: true,
+    expires,
   }
   res.cookie("auth", generateAuthToken(auth), options)
 }
