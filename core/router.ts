1550b400e084996cca1d0154367c4e1823b1769c
Noel Carmona <hello@noelcarmona.com>
2022-07-13 12:19:28 +0200

    [ROUTER] Added test router in router.

--- core/router.ts
+++ core/router.ts
@@ -1,8 +1,10 @@
 import { Application } from "express"
 import { AuthRoutes } from "../routes/authRoutes"
 import { UserRoutes } from "@routes/userRoutes"
+import { TestRoutes } from "@routes/testRoutes"
 
 export function router(core: Application) {
   new AuthRoutes(core, "/auth")
   new UserRoutes(core, "/user")
+  if (process.env.ENVIRONMENT === "test") new TestRoutes(core, "/test")
 }
