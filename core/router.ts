c562bd20c8f543dab1067c44d5f5b78ab22095be
Noel Carmona <hello@noelcarmona.com>
2022-07-20 18:37:25 +0200

    [ROUTER] Enable options methods for all requests.

--- core/router.ts
+++ core/router.ts
@@ -2,8 +2,11 @@ import { Application } from "express"
 import { AuthRoutes } from "../routes/authRoutes"
 import { UserRoutes } from "@routes/userRoutes"
 import { TestRoutes } from "@routes/testRoutes"
-
+import cors from "cors"
+import { allowAll } from "./cors"
 export function router(core: Application) {
+  core.options("*", cors(allowAll))
+
   new AuthRoutes(core, "/auth")
   new UserRoutes(core, "/user")
   if (process.env.ENVIRONMENT === "test") new TestRoutes(core, "/test")
