524c894e6d4b166f7aa6c52e5e8a7f0347ffc7ff
Noel Carmona <hello@noelcarmona.com>
2022-09-27 18:04:15 +0200

    [INTERFACE] Board interface

---
+++ domain/interfaces/IBoard.ts
@@ -0,0 +1,9 @@
+import { Model } from "../../core/models/model"
+import { ModelDeleteDisable } from "../../core/models/modelDeleteDisable"
+import { ModelDataAccess } from "../../core/models/modelDataAccess"
+import { IUser } from "./IUser"
+
+export interface IBoard extends ModelDataAccess, ModelDeleteDisable, Model {
+  name: string
+  owner: IUser | string
+}
