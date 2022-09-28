0c50a85bfd556910fcea087a3381725a2abe4657
Noel Carmona <hello@noelcarmona.com>
2022-09-28 21:28:40 +0200

    [BOARD] Added disabled, deleted, modified and created to schema

--- core/repository/MongoDB/Schemas/Board.ts
+++ core/repository/MongoDB/Schemas/Board.ts
@@ -9,6 +9,22 @@ const schema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  disabled: {
+    type: Boolean,
+    default: true,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  modified_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
 })
 const mongoDBBoard = model("Board", schema)
 export { mongoDBBoard }
