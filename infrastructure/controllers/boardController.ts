49289b52c20ce5d9b31c70ee208e1dd99f553a92
Noel Carmona <hello@noelcarmona.com>
2022-09-28 21:24:54 +0200

    [BOARD] Create controller handles messages improvment

--- infrastructure/controllers/boardController.ts
+++ infrastructure/controllers/boardController.ts
@@ -14,9 +14,21 @@ export class BoardController {
     this.boardUseCases = new BoardUseCases(this.mongoDBBoardRepository)
   }
   async create(name: string, user: string): Promise<IResponse> {
-    const board: BoardModel = await this.boardUseCases.create(name, user)
+    const board: BoardModel | null = await this.boardUseCases.create(name, user)
+    let response: IResponse
+    if (board !== null) {
+      const boardObject: IBoard = board.getModel()
+      response = createBoardSuccessful(boardObject)
+    } else response = boardAlreadyExistsShowName(name)
+
+    return response
+  }
+  async get(name: string): Promise<IResponse> {
+    const formatedName: string = name.replace(/-/g, " ")
+    const board: BoardModel = await this.boardUseCases.get(formatedName)
     const boardObject: IBoard = board.getModel()
 
+    return retrievedBoard(boardObject)
   }
   async exists(name: string): Promise<IResponse> {
     const formatedName: string = name.replace(/-/g, " ")
