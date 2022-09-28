33b0b1d5ca5e3dcf3e1692ea93414a16a7089f78
Noel Carmona <hello@noelcarmona.com>
2022-09-28 21:21:54 +0200

    [BOARD] Check if board with same name exists

--- infrastructure/repositories/boardRepository/mongoDBBoardRepository.ts
+++ infrastructure/repositories/boardRepository/mongoDBBoardRepository.ts
@@ -12,17 +12,36 @@ export class MongoDBBoardRepository implements BoardRepository {
   constructor() {
     this.connector = new MongoDBConnector()
     this.connector.link()
+    this.hiddenFieldsBoard = ["-_id"]
+    this.hiddenFieldsUser = [
+      "-password",
+      "-email",
+      "-modified_at",
+      "-disabled",
+      "-deleted",
+      "-activation_token",
+    ]
   }
-  create(name: string, user: string): Promise<BoardModel> {
-    return new Promise((resolve, reject) => {
-      const board: IBoard = {
-        created_at: new Date(),
-        deleted: false,
-        disabled: false,
-        modified_at: new Date(),
-        name,
-        owner: user,
-      }
+  async create(name: string, user: string): Promise<BoardModel> {
+    const board: IBoard = {
+      created_at: new Date(),
+      deleted: false,
+      disabled: false,
+      modified_at: new Date(),
+      name,
+      owner: user,
+    }
+
+    try {
+      const exists = await this.exists(name)
+      if (!exists) {
+        const retrievedBoard = await mongoDBBoard.create(board)
+        return new BoardModel(retrievedBoard)
+      } else return null
+    } catch (error) {
+      return error
+    }
+  }
   async exists(board: string): Promise<boolean> {
     const filter: FilterQuery<unknown> = { name: board }
     try {
